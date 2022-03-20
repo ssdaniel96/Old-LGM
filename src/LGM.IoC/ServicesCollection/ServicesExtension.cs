a54602d3fee691da60a224e4c1af9b9972ce8fbd
Daniel da Silva Soares <daniel_soares2007@hotmail.com>
2022-03-20 13:29:16 -0300

    appSettings and ioc implementation

---
+++ src/LGM.IoC/ServicesCollection/ServicesExtension.cs
@@ -0,0 +1,68 @@
+﻿using LGM.Adapter.Repositories.HealthChecks;
+using LGM.Application.Mappings.Groups;
+using LGM.DTOS.Options;
+using LGM.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace LGM.IoC.ServicesCollection
+{
+    public static class ServicesExtension
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDatabase(configuration)
+                .AddOptions(configuration)
+                .AddMappers()
+                .AddDependencies();
+
+            return services;
+        }
+
+        private static IServiceCollection AddMappers(this IServiceCollection services) =>
+            services.AddAutoMapper(typeof(DomainToDtoMappingProfile).Assembly);
+
+        private static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            services.Scan(scan => {
+                var assemblies = new Assembly[] {
+                    Assembly.GetEntryAssembly(), // LGM.Api
+                    typeof(IHealthCheckRepository).Assembly, //LGM.Adapter
+                    typeof(DomainToDtoMappingProfile).Assembly, //LGM.Application
+                    typeof(ApplicationDbContext).Assembly //LGM.Repository
+                };
+
+                scan.FromAssemblies(assemblies)
+                    .AddClasses()
+                    .AsMatchingInterface()
+                    .WithScopedLifetime();
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ApiVersionOptionDto>(v => {
+                v.Version = configuration.GetSection("ApiVersion").Value;
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                opt =>
+                    opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                    .EnableSensitiveDataLogging()
+                    .LogTo(Console.WriteLine, LogLevel.Information));
+
+            return services;
+        }
+    }
+}
